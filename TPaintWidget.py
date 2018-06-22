2226d185c207ff6b66a36317951c7e1dcb022152
masanori-komehana <s14524@salesio-sp.ac.jp>
2018-06-22 14:23:46 +0900

    exampleで画像読み込んでその上からペン入力できるやつがあったので引っ張ってきた

--- TPaintWidget.py
+++ TPaintWidget.py
@@ -17,18 +17,23 @@ class TPaintWidget(Qw.QWidget):
         canvas = Qg.QPainter(self)
         self.onPaint.emit(event, canvas)                #シグナルの発行
         print("paint")
+        print(dir(event))
 
     def mousePressEvent(self, event: Qg.QMouseEvent):
         self.onMousePress.emit(event)
         print("press")
+        print(dir(event))
 
     def mouseMoveEvent(self, event: Qg.QMouseEvent):
         self.onMouseMove.emit(event)
         print("move")
+        print(dir(event))
 
     def mouseReleaseEvent(self, event: Qg.QMouseEvent):
         self.onMouseRelease.emit(event)
         print("release")
+        print(dir(event))
 
     def mouseDoubleClickEvent(self, event: Qg.QMouseEvent):
         self.onDoubleClick.emit(event)
+        print(dir(event))
