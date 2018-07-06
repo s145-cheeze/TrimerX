cd46b829582dda67c29619ff7f14e5610712f623
masanori-komehana <s14524@salesio-sp.ac.jp>
2018-07-06 14:25:22 +0900

    追加：座標取得したり，その中の最大・最小座標を取ってきたりする

--- FreeTrim.py
+++ FreeTrim.py
@@ -50,10 +50,106 @@ class FreeTrim(TPaintWidget):
         self.hbox.addWidget(self.lbl)
         self.setLayout(self.hbox)
 
+        self.setAttribute(Qt.WA_StaticContents)
+        self.modified = False
+        self.scribbling = False
+        self.myPenWidth = 1
+        self.myPenColor = Qt.blue
+        self.image = QImage()
+        self.lastPoint = QPoint()
+
+        self.minX = 0
+        self.minY = 0
+        self.maxX = 0
+        self.maxY = 0
+
+        self.currentRect = FreeTrimRect(self.minX, self.minY, self.maxX, self.maxY)
+        self.rects = []
+
+        self.openImage(self.fname[0])
+
+    def updateRectByXY(self, x, y):
+        self.minX = min(self.minX, x)
+        self.minY = min(self.minY, y)
+        self.maxX = max(self.maxX, x)
+        self.maxY = max(self.maxY, y)
+        self.currentRect.update(self.minX, self.minY, self.maxX, self.maxY)
+
+    def updateRectByQPoint(self, pos):
+        self.updateRectByXY(pos.x(), pos.y())
+
+
+    def newRect(self):
+        self.minX = 0
+        self.minY = 0
+        self.maxX = 0
+        self.maxY = 0
+
+        self.rects.append(self.currentRect)
+
+        self.currentRect = FreeTrimRect(self.minX, self.minY, self.maxX, self.maxY)
+
+
     def showDialog(self):
         return QFileDialog.getOpenFileName(parent = self, caption = "OpenFile" , filter ='*.jpg' )
 
+    def openImage(self, fileName):
+        loadedImage = QImage()
+        if not loadedImage.load(fileName):
+            return False
 
+        newSize = loadedImage.size().expandedTo(self.size())
+        self.resizeImage(loadedImage, newSize)
+        self.image = loadedImage
+        self.modified = False
+        self.update()
+        return True
+
+    def resizeImage(self, image, newSize):
+        if image.size() == newSize:
+            return
+
+        newImage = QImage(newSize, QImage.Format_RGB32)
+        newImage.fill(qRgb(255, 255, 255))
+        painter = QPainter(newImage)
+        painter.drawImage(QPoint(0, 0), image)
+        self.image = newImage
+
+    def mousePressEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            self.lastPoint = event.pos()
+            self.scribbling = True
+            self.updateRectByQPoint(event.pos())
+
+    def mouseMoveEvent(self, event):
+        if (event.buttons() & Qt.LeftButton) and self.scribbling:
+            self.drawLineTo(event.pos())
+            print(event.pos())
+            print(event.pos().x(), event.pos().y())
+            self.updateRectByQPoint(event.pos())
+
+    def mouseReleaseEvent(self, event):
+        if event.button() == Qt.LeftButton and self.scribbling:
+            self.drawLineTo(event.pos())
+            self.scribbling = False
+            self.updateRectByQPoint(event.pos())
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        dirtyRect = event.rect()
+        painter.drawImage(dirtyRect, self.image, dirtyRect)
+
+
+    def drawLineTo(self, endPoint):
+        painter = QPainter(self.image)
+        painter.setPen(QPen(self.myPenColor, self.myPenWidth, Qt.SolidLine,
+        Qt.RoundCap, Qt.RoundJoin))
+        painter.drawLine(self.lastPoint, endPoint)
+        self.modified = True
+
+        rad = self.myPenWidth / 2 + 2
+        self.update(QRect(self.lastPoint, endPoint).normalized().adjusted(-rad, -rad, +rad, +rad))
+        self.lastPoint = QPoint(endPoint)
 
 if __name__ == '__main__':
     main()
