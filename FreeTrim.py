5c0592854754a6c4f387d3edb122992ce28326f5
masanori-komehana <s14524@salesio-sp.ac.jp>
2018-07-06 14:23:41 +0900

    追加：トリミング用する矩形領域を扱うクラス

--- FreeTrim.py
+++ FreeTrim.py
@@ -3,7 +3,12 @@
 import sys
 
 from PyQt5 import QtWidgets, QtGui
-from PyQt5.QtWidgets import QWidget, QMessageBox, QApplication, QFileDialog
+
+from PyQt5.QtCore import QDir, QPoint, QRect, QSize, Qt
+from PyQt5.QtGui import QImage, QImageWriter, QPainter, QPen, qRgb
+from PyQt5.QtWidgets import (QAction, QApplication, QColorDialog, QFileDialog,
+        QInputDialog, QMainWindow, QMenu, QMessageBox, QWidget)
+from PyQt5.QtPrintSupport import QPrintDialog, QPrinter
 
 from TPaintWidget import *
 
@@ -13,6 +18,23 @@ def main():
     ex1.show()
     sys.exit(app.exec_())
 
+
+class FreeTrimRect(object):
+        """docstring for FreeTrimRect."""
+        def __init__(self, *args):
+            super(FreeTrimRect, self).__init__()
+            self.update(*args)
+        def get(self):
+            return self.p1, self.p2
+        def update(self,*args):
+            if len(args) == 4:
+                self.p1 = args[0], args[1]
+                self.p2 = args[2], args[3]
+            elif len(args) == 2:
+                self.p1 = args[0]
+                self.p2 = args[1]
+        def __str__(self):
+            return "FreeTrimRect<{},{}>".format(str(self.p1), str(self.p2))
 
 
 class FreeTrim(TPaintWidget):
