c166f026207d13f9fd0d6b15dc9cb15cf8b0f4d2
masanori-komehana <s14524@salesio-sp.ac.jp>
2019-01-18 01:02:47 +0900

    追加：各生徒にぶら下げる，解答を管理するクラス(AnswerManagerクラス)

--- AnsChk/AnswerManager.py
+++ AnsChk/AnswerManager.py
@@ -1,7 +1,19 @@
 # -*- coding: utf-8 -*-
 
+from Anschk.Answer import *
 
 class AnswerManager(object):
-    """docstring for AnswerManager."""
-    def __init__(self, arg):
-        self.arg = arg
+    def __init__(self, ansmngdata):
+        self.ansmngdata = ansmngdata
+        self.judges = [0 for i in range(len(ansmngdata))]
+        self.answers = [Answer(*data) for data in self.ansmngdata.gets()]
+    def gets(self):
+        for cnt, t in enumerate(self.ansmngdata.gets()):
+            number, scoring = t
+            judge = self.judges[cnt]
+            yield number, scoring, judge
+    def get(self, index):
+        number, scoring = self.ansmngdata.get(index)
+        return number, scoring, self.judges[index]
+    def getAnswer(self, index):
+        return self.answers[index]
