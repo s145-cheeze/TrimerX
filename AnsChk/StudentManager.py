1232781d8dde527330af416728f02f3d183a607e
masanori-komehana <s14524@salesio-sp.ac.jp>
2019-01-18 01:05:01 +0900

    追加：各生徒の解答を管理するクラス(StudentManager)

--- AnsChk/StudentManager.py
+++ AnsChk/StudentManager.py
@@ -1,6 +1,25 @@
 # -*- coding: utf-8 -*-
 
+from AnsChk.Student import *
+from AnsChk.AnswerManagerFactory import *
+
 class StudentManager(object):
-    """docstring for StudentManager."""
-    def __init__(self, arg):
-        self.arg = arg
+    def __init__(self, ansmngdata):
+        self.students = []
+        self.ansmngfactory = AnswerManagerFactory(ansmngdata)
+    def create(self, number, name):
+        ansmng = self.ansmngfactory.create()
+        student  = Student(ansmng, number, name)
+        self.students
+    def gets(self):
+        for student in self.students:
+            yield student
+    def get(self, index):
+        return self.students[index]
+    @staticmethod
+    def fromCSV(ansmngdata, fname):
+        smng = StudentManager(ansmngdata)
+        with open(fname,"r") as f:
+            reader = csv.reader(f)
+            for row in reader:
+                smng.create(*row)
