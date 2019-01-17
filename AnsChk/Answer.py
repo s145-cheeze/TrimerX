6aaa24e5b7cc9321ed62cf351ad2ed20aeb6ee0b
masanori-komehana <s14524@salesio-sp.ac.jp>
2019-01-17 15:30:17 +0900

    実装；１問の解答を管理するクラス(Answerクラス)

--- AnsChk/Answer.py
+++ AnsChk/Answer.py
@@ -2,7 +2,25 @@
 
 
 class Answer(object):
-    """docstring for Answer."""
-    def __init__(self, arg):
-        super(Answer, self).__init__()
-        self.arg = arg
+    def __init__(self, number, scoring, judge = 0, img = None):
+        # 問番号
+        self.number = number
+        # 配点
+        self.scoring = scoring
+        # 判定
+        self.judge = judge
+        # 回答画像
+        self.img = img
+    def setJudge(self, judge):
+        self.judge = judge if judge <= self.scoring else self.scoring
+        if self.judge < 0 : self.judge = 0
+    def setImg(self, img):
+        self.img = img
+    def getNumber(self):
+        return self.number
+    def getImg(self):
+        return self.img
+    def getScoring(self):
+        return self.scoring
+    def getJudge(self):
+        return self.judge
